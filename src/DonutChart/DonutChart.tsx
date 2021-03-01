2cc0bc2544409e9184ac40b857a5aa0b88c53d19
Aleksey Fedorov <aleksey.fedorov@csssr.com>
2021-03-01 14:38:35 +0700

    feat(components): добавлена возможность указывать сумму сегментов в виджете DonutChart

--- src/DonutChart/DonutChart.tsx
+++ src/DonutChart/DonutChart.tsx
@@ -10,24 +10,29 @@ import { HalfDonut } from '@/__private__/components/CoreDonutChart/CoreDonutChar
 import { Data as DonutTextData } from '@/__private__/components/CoreDonutChart/CoreDonutChartText/CoreDonutChartText'
 import { FormatValue } from '@/__private__/types'
 
+import { filterComputedData, getComputedData } from './helpers'
+
 type Props = {
   data: readonly DonutData[]
   textData?: DonutTextData
   halfDonut?: HalfDonut
   valueSize?: number
+  sums?: readonly number[]
   formatValueForTooltip?: FormatValue
   sortValue?: SortValue | null
 }
 
-export const DonutChart: React.FC<Props> = ({ halfDonut, ...rest }) => {
+export const DonutChart: React.FC<Props> = ({ data, sums, halfDonut, ...rest }) => {
   return (
     <CoreDonutChart
       {...rest}
+      data={getComputedData(data, sums)}
       halfDonut={halfDonut}
       titlePosition={halfDonut === 'bottom' ? 'bottom' : 'top'}
       textPaddingFromBorder={halfDonut ? 8 : 0}
       showTitle={Boolean(halfDonut)}
       showSubBlock={isHalfDonutVertical(halfDonut)}
+      filterTooltipItem={filterComputedData}
       showTooltip
       showText
     />
