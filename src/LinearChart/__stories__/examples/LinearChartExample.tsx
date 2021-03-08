6a4867bedf3b304726faa1693e58e28dea53c48b
Anya Ovsyannikova <arhayka@gmail.com>
2021-03-08 08:29:42 +0300

    docs(LinearChart): docs(LinearChart): docs(LinearChart): docs(LinearChart): edit docs for LinearChart

--- src/LinearChart/__stories__/examples/LinearChartExample.tsx
+++ src/LinearChart/__stories__/examples/LinearChartExample.tsx
@@ -1,8 +1,11 @@
-// import React from 'react'
-//
-// import { Example } from '@/_private/storybook'
-//
-// import { LinearChart } from '../../LinearChart'
+import React from 'react'
+
+import { Example } from '@/__private__/storybook'
+
+// import { Stats } from '../..'
+
+import { LinearChart } from '../../LinearChart'
+
 // import {
 //   gridConfigFormat,
 //   gridConfigLabel,
@@ -18,170 +21,135 @@
 //   threshold,
 // } from '../data.mock'
 //
-// export const LinearChartExampleWithoutGradient = () => (
+
+const colors = {
+  first: 'var(--color-bg-success)',
+  second: 'var(--color-bg-normal)',
+}
+
+const lines = [
+  {
+    values: [
+      { x: 1, y: 1 },
+      { x: 2, y: 2 },
+      { x: 3, y: 4 },
+      { x: 4, y: 4 },
+      { x: 5, y: 3 },
+    ],
+    dots: false,
+    lineName: 'Линия жизни',
+    withGradient: false,
+    color: colors.first,
+  },
+  {
+    values: [
+      { x: 0, y: 2 },
+      { x: 1, y: 4 },
+      { x: 3, y: 5 },
+    ],
+    dots: false,
+    lineName: 'Линия любви',
+    withGradient: false,
+    color: colors.second,
+  },
+]
+
+const linesBig = [
+  {
+    values: [
+      { x: 0, y: 0 },
+      { x: 1, y: 1 },
+      { x: 2, y: 0 },
+      { x: 5, y: 3 },
+      { x: 7, y: 1 },
+      { x: 9, y: 3 },
+    ],
+    dots: true,
+    dashed: true,
+    showValues: true,
+    lineName: 'Красивая линия',
+    withGradient: true,
+    color: 'var(--color-bg-normal)',
+  },
+]
+
+const linesNu = [
+  {
+    values: [
+      { x: 0, y: null },
+      { x: 1, y: 1 },
+      { x: 2, y: 0 },
+      { x: 3, y: null },
+      { x: 4, y: null },
+      { x: 5, y: 3 },
+      { x: 6, y: null },
+      { x: 7, y: 1 },
+      { x: 8, y: 2 },
+      { x: 9, y: null },
+    ],
+    dots: true,
+    showValues: true,
+    lineName: 'Красивая линия',
+    withGradient: true,
+    color: 'var(--color-bg-normal)',
+  },
+]
+
+const gridConfig = {
+  x: {
+    min: -1,
+    max: 6,
+    showGuide: true,
+    withPaddings: false,
+  },
+  y: {
+    min: -1,
+    max: 6,
+    showGuide: true,
+    withPaddings: false,
+  },
+}
+
+// const legend = {
+//   items: [
+//     {
+//       color: colors.first,
+//       text: 'Успех',
+//     },
+//     {
+//       color: colors.second,
+//       text: 'Болъ',
+//     },
+//   ],
+//   align: 'right',
+// }
+
+// export const LinearChartExampleLegend = () => (
 //   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       // title="Название графика"
-//       lines={linesWithoutGradient}
-//       gridConfig={gridConfigSimple}
-//       yDimensionUnit="единицы"
-//     />
+//     <LinearChart lines={lines} gridConfig={gridConfig} legend={legend} />
 //   </Example>
 // )
-//
-// export const LinearChartExampleLinesFeatures = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       // title="Название графика"
-//       lines={linesFeatures}
-//       gridConfig={gridConfigPaddings}
-//       yDimensionUnit="единицы"
-//     />
-//   </Example>
-// )
-//
-// export const LinearChartExampleGeneral = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       // title="Название графика"
-//       lines={linesSimple}
-//       gridConfig={gridConfigSimple}
-//       yDimensionUnit="единицы"
-//     />
-//   </Example>
-// )
-//
-// export const LinearChartExampleFormatLabelTooltip = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       yDimensionUnit="км"
-//       lines={linesFormat}
-//       gridConfig={gridConfigFormat}
-//       formatValueForLabel={v => new Date(v).toLocaleDateString()}
-//       formatValueForTooltip={v => `${v} км`}
-//       formatValueForTooltipTitle={v => {
-//         const title = new Date(v)
-//           .toLocaleDateString('ru-RU', { month: 'long', year: 'numeric' })
-//           .replace('г.', '')
-//         return title[0].toUpperCase() + title.slice(1)
-//       }}
-//     />
-//   </Example>
-// )
-//
-// export const LinearChartExampleFormatLabelData = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       lines={linesFormat}
-//       gridConfig={gridConfigFormat}
-//       formatValueForLabel={v => new Date(v).toLocaleDateString()}
-//     />
-//   </Example>
-// )
-//
-// export const LinearChartExampleFormatLabelProcent = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       lines={linesSimple}
-//       gridConfig={gridConfigSimple}
-//       formatValueForLabel={v => `${v} %`}
-//     />
-//   </Example>
-// )
-//
-// /**
-//  * Пример пока отключен так как проявляется проблема с расчетом
-//  * дробных px в компоненте Axis.
-//  */
-// /*
-// export const LinearChartExampleNotHorizontal = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       title="Очень красивый график"
-//       lines={linesSimple}
-//       gridConfig={gridConfigSimple}
-//       unit="км"
-//       background="linear-gradient(to right, #f54d4d48, transparent)"
-//       isHorizontal={false}
-//     />
-//   </Example>
-// )
-// */
-//
-// export const LinearChartExampleLabel = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       // title="Очень красивый график"
-//       lines={linesSimple}
-//       gridConfig={gridConfigLabel}
-//       yDimensionUnit="км"
-//     />
-//   </Example>
-// )
-//
-// export const LinearChartExampleDirectionXtoLeft = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart lines={linesSimple} gridConfig={gridConfigSimple} />
-//   </Example>
-// )
-//
-// export const LinearChartExampleDirectionXtoRight = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart lines={linesSimple} gridConfig={gridConfigSimple} />
-//   </Example>
-// )
-//
-// export const LinearChartExampleDirectionYtoTop = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart lines={linesSimple} gridConfig={gridConfigSimple} />
-//   </Example>
-// )
-//
-// export const LinearChartExampleDirectionYtoBottom = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart lines={linesSimple} gridConfig={gridConfigSimple} yDimensionUnit="км" />
-//   </Example>
-// )
-//
-// export const LinearChartExampleNull = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart lines={linesNull} gridConfig={gridConfigSimple} />
-//   </Example>
-// )
-//
-// export const LinearChartExampleBoundaries = () => (
-//   <Example width="600px" height="300px" margin="l">
-//     <LinearChart lines={linesBoundaries} gridConfig={gridConfigSimple} />
-//   </Example>
-// )
-//
-// export const LinearChartExampleZoom = () => (
-//   <Example width="300px" height="150px" margin="l">
-//     <LinearChart
-//       // title="Очень красивый график"
-//       lines={linesSimple}
-//       gridConfig={gridConfigSimple}
-//       yDimensionUnit="км"
-//     />
-//   </Example>
-// )
-//
-// export const LinearChartExampleThreshold = () => (
-//   <Example width="700px" height="300px" margin="l">
-//     <LinearChart
-//       // title="График с предельными значениями"
-//       lines={linesThreshold}
-//       gridConfig={gridConfigSimple}
-//       threshold={threshold}
-//       yDimensionUnit="тыс. м3"
-//       formatValueForLabel={v => new Date(v).toLocaleDateString()}
-//       formatValueForTooltip={v => `${v} тыс м3`}
-//       formatValueForTooltipTitle={v => {
-//         const title = new Date(v)
-//           .toLocaleDateString('ru-RU', { month: 'long', year: 'numeric' })
-//           .replace('г.', '')
-//         return title[0].toUpperCase() + title.slice(1)
-//       }}
-//     />
-//   </Example>
-// )
+
+export const LinearChartExampleyDimensionUnit = () => (
+  <Example width="300px" height="150px" margin="l">
+    <LinearChart lines={lines} gridConfig={gridConfig} yDimensionUnit="единицы" />
+  </Example>
+)
+
+export const LinearChartExampleLines = () => (
+  <Example width="300px" height="150px" margin="l">
+    <LinearChart lines={linesBig} gridConfig={gridConfig} />
+  </Example>
+)
+
+export const LinearChartExampleLinesNull = () => (
+  <Example width="300px" height="150px" margin="l">
+    <LinearChart lines={linesNu} gridConfig={gridConfig} />
+  </Example>
+)
+
+export const LinearChartExamplePercentY = () => (
+  <Example width="300px" height="150px" margin="l">
+    <LinearChart lines={lines} gridConfig={gridConfig} yLabelsShowInPercent />
+  </Example>
+)
