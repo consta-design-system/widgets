d0e508c13c6d1cf586086f6aafec7cd7a353aed7
filipp.karpov <filipp.karpov@nedra.digital>
2021-04-02 13:32:25 +0300

    feat(Tooltip): updated tooltip

--- src/__private__/components/TooltipContentForMultipleValues/TooltipContentForMultipleValues.tsx
+++ src/__private__/components/TooltipContentForMultipleValues/TooltipContentForMultipleValues.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import { Text } from '@consta/uikit/Text'
+import { isNotNil } from '@consta/widgets-utils/lib/type-guards'
 import _ from 'lodash'
 
 import { FormatValue } from '@/__private__/types'
 import { cn } from '@/__private__/utils/bem'
 import { getFormattedValue } from '@/__private__/utils/chart'
 import { numberFormatter } from '@/__private__/utils/formatters'
+import { formatForArray } from '@/__private__/utils/formatForArray'
 import { LegendItem } from '@/LegendItem/LegendItem'
 
 import './TooltipContentForMultipleValues.css'
@@ -30,6 +32,11 @@ export const TooltipContentForMultipleValues: React.FC<Props> = ({
   items,
   formatValueForTooltip,
 }) => {
+  const newItems = items.map(item => item.value || 0)
+  const formatItems = formatForArray(newItems)
+  const withNoNames = items.every(item => !item.name)
+  const withNoColors = items.every(item => !item.color)
+
   return (
     <div className={cnTooltipContent('Container')}>
       {title && (
@@ -43,31 +50,43 @@ export const TooltipContentForMultipleValues: React.FC<Props> = ({
           >
             {title}
           </Text>
-          <div className={cnTooltipContent('Divider')} />
         </>
       )}
 
       <div className={cnTooltipContent('Content')}>
         {items.map(({ name, color, value }, idx) => {
-          const formattedValue = numberFormatter(
-            getFormattedValue(value ?? null, formatValueForTooltip)
-          )
+          const formattedValue = getFormattedValue(value ?? null, formatValueForTooltip)
+          const newFormattedValue = isNotNil(value)
+            ? formatItems[idx] +
+              ' ' +
+              formattedValue
+                .split(' ')
+                .slice(1)
+                .join(' ')
+            : formattedValue
 
           return (
             <React.Fragment key={idx}>
               <LegendItem
-                label={name ?? formattedValue}
+                label={name || ''}
                 icon={_.isNumber(value) ? 'dot' : undefined}
                 color={color}
                 size="xs"
                 className={cnTooltipContent('LegendItem', {
-                  isSingleColumn: !name,
-                  withPadding: !color,
+                  isSingleColumn: !name && !value,
+                  withPadding: !color && !withNoColors,
+                  withNoNames,
                 })}
               />
-              {name && (
-                <Text as="span" size="xs" weight="bold" view="primary">
-                  {formattedValue}
+              {(name || value) && (
+                <Text
+                  className={cnTooltipContent('ItemValue')}
+                  as="span"
+                  size="xs"
+                  weight={withNoNames ? 'regular' : 'bold'}
+                  view="primary"
+                >
+                  {numberFormatter(newFormattedValue)}
                 </Text>
               )}
             </React.Fragment>
