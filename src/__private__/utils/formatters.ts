8eb9d934c7dec603876b69fd6fea7d6b1cf159d6
gizeasy <gizeasy@gmail.com>
2022-04-26 14:10:32 +0300

    fix(numberFormatter): fixed regex

--- src/__private__/utils/formatters.ts
+++ src/__private__/utils/formatters.ts
@@ -2,6 +2,6 @@ import { DECIMAL_SEPARATOR, NARROW_NO_BREAK_SPACE } from './symbols'
 
 export const numberFormatter = (value: string | number) => {
   return String(value)
-    .replace(/(\d)(?=(\d{3})+\b)(?<!\.\d+)/g, `$1${NARROW_NO_BREAK_SPACE}`)
-    .replace(/()\.()/g, DECIMAL_SEPARATOR)
+    .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, `$1${NARROW_NO_BREAK_SPACE}`)
+    .replace(/(?=.\d)\.(?=\d+)/g, DECIMAL_SEPARATOR)
 }
