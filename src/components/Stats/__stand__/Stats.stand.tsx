ac0bab9d9b1053e48f6533bdbd7e9ff9356454dc
Anya Ovsyannikova <arhayka@gmail.com>
2022-09-07 11:32:22 +0300

    docs: edit docs, add images

--- src/components/Stats/__stand__/Stats.stand.tsx
+++ src/components/Stats/__stand__/Stats.stand.tsx
@@ -1,10 +1,13 @@
 import { createStand } from '##/stand/standConfig';
 
+import image from './Stats.image.svg';
+
 export default createStand({
   title: 'Stats',
   id: 'Stats',
+  image,
   group: 'components',
-  description: 'Изменение значений.',
+  description: 'Важные числа и их изменение.',
   version: '4.2.2',
   status: 'stable',
 });
