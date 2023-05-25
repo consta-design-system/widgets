552f5c1cb9cf43a580c843fac73612c9faea1dae
gizeasy <gizeasy@gmail.com>
2023-05-25 18:00:42 +0300

    docs(standConfig): add figmaUrl

--- src/stand/standConfig.ts
+++ src/stand/standConfig.ts
@@ -17,6 +17,8 @@ export const { createStand } = createConfig({
   image,
   description: 'Графики и диаграммы для дизайн-системы Consta',
   repositoryUrl: 'https://github.com/consta-design-system/widgets',
+  figmaUrl:
+    'https://www.figma.com/file/sMFxkiJmRwf922V7ll6DmP/Consta-Widgets-(Community)?type=design&node-id=0-1&t=zobycrV12ZLeeoLx-0',
   order: 30,
   status: 'deprecated',
 });
